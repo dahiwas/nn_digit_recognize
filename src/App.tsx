64fec3a630f753751bf22b68257afa8c0d7d08d2
Daniel <dahirowata@gmail.com>
2025-01-09 17:52:37 -0300

    Arrumado

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="Container">
       <iframe
-        src="http://129.148.39.226:8501"
+        src="https://129.148.39.226:8501"
         title="Streamlit App"
       ></iframe>
     </div>
